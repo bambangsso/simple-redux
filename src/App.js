e3f97d17fceabebba4424b42799d05a8c20b214b
bambang Susilo <bambang.sso@gmail.com>
2020-10-26 07:39:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const center = {
+  display: 'flex',
+  'justify-content': 'center',
+  'align-items': 'center',
+  height: '100vh'
 }
 
-export default App;
+
+export default class  App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  goUp = () => {
+    this.setState({counter: this.state.counter + 1})
+  } 
+
+  goDown = () => {
+    this.setState({counter: this.state.counter - 1})
+  }   
+
+  render() {
+    return (
+      <div style={center}>
+        <button onClick={() => this.goUp()}>+</button>
+        {this.state.counter}
+         <button onClick={() => this.goDown()}>-</button>
+      </div>
+    )
+  }
+};
